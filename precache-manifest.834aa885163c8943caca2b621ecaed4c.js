f97e2b8cac2ab7dedcbe2e130e2f44009b623b4f
Curly Joe <curlyjoe71@gmail.com>
2018-12-20 01:29:17 -0700

    Updates

---
+++ precache-manifest.834aa885163c8943caca2b621ecaed4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24c1d7e61a3d8a025c6a",
+    "url": "/clicky-memory-game/static/js/runtime~main.24c1d7e6.js"
+  },
+  {
+    "revision": "8bc17973ca1939ab90a9",
+    "url": "/clicky-memory-game/static/js/main.8bc17973.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clicky-memory-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "8bc17973ca1939ab90a9",
+    "url": "/clicky-memory-game/static/css/main.929b99c9.chunk.css"
+  },
+  {
+    "revision": "6021b9e4cbd394f074e18d1b4c320116",
+    "url": "/clicky-memory-game/index.html"
+  }
+];
